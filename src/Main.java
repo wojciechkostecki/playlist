cdd069b4c99cbd7f7573c0426d564cbc17dc2513
Wojciech Kostecki <wt.kostecki@gmail.com>
2020-04-16 19:53:05 +0200

    Creating a method that changes seconds into the HH:MM:SS format

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,8 @@ public class Main {
         Song song1 = new Song("BUBBLETEA", 284);
         Song song2 = new Song("Ground Zero", 178);
         Song song3 = new Song("Chcemy być wyżej", 186);
+        Song song4 = new Song("Nie byłem nigdy", 179);
 
-        System.out.println(playlist.playlistLength(song1,song2,song3));
-
+        playlist.printPlaylistDuration(playlist.playlistLength(song1,song2,song3, song4));
     }
 }
--- src/Playlist.java
+++ src/Playlist.java
@@ -1,3 +1,7 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
 public class Playlist {
 
     public int playlistLength(Song... songs ){
@@ -6,5 +10,13 @@ public class Playlist {
             playingTime += song.getDuration();
         }
         return playingTime;
+    }
+
+    public void printPlaylistDuration(int durationSong){
+        Date d = new Date(durationSong * 1000L);
+        SimpleDateFormat df = new SimpleDateFormat("HH:mm:ss");
+        df.setTimeZone(TimeZone.getTimeZone("GMT"));
+        String time = df.format(d);
+        System.out.println(time);
     }
 }
